e81e30a01d1f84e4ebef32a78f514afa62f9ffea
GM <gmorin@digitalguardian.com>
2018-08-03 10:36:21 -0400

    new c file

---
+++ code/c.c
@@ -0,0 +1,32 @@
+#include <test1>
+#include <test2>
+#include <test3>
+#include <test4>
+#include <test5>
+#include <test6>
+#include <test7>
+#include <test8>
+
+using file.io;
+using file2.io;
+
+
+struct {
+	int testInt = 0;
+	String testString = "test";
+	Boolean testBool = True;
+}
+
+
+
+int main() {
+	printf("Hello, World);
+	return 0;
+}
+
+
+
+
+/* example company ip */
+
+
